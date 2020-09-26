2a1ef1199bd07629a622a613eeef27baedbba6db
Aryan Jha <iamaryanjha@gmail.com>
2020-09-26 14:36:57 +0530

    added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Line2 from '../../components/Line2.png'
 
 class Navbar extends Component {
     render() {
         return (
             <div>
-                
+                 <a href ="/">ARYAN JHA</a>
             </div>
         )
     }
