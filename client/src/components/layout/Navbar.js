9becb500838cb2cd59f263c65fae8d6f0613057a
Aryan Jha <iamaryanjha@gmail.com>
2020-09-27 22:45:25 +0530

    navbar added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import Line2 from '../../components/Line2.png'
+import { makeStyles } from '@material-ui/core/styles';
+import {AppBar,Toolbar,Typography,Button,IconButton} from '@material-ui/core';
 
-class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                 <a href ="/">ARYAN JHA</a>
-            </div>
-        )
-    }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  logo: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="secondary">
+        <Toolbar>
+          <IconButton edge="start" className={classes.logo} color="inherit" aria-label="menu">
+            <img src={Line2} width="250px"></img>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}/>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-
-export default Navbar;
